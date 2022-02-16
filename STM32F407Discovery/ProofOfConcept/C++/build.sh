1c9783be56e71732dc6fd6bed78b27f10a0433c9
Arsha <arsha.shiri@gmail.com>
2022-02-16 20:03:20 +0100

    Try debugging the POC C++ project

--- STM32F407Discovery/ProofOfConcept/C++/build.sh
+++ STM32F407Discovery/ProofOfConcept/C++/build.sh
@@ -8,3 +8,8 @@ fi
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -S ./ -B $buildDir
 cd $buildDir
 make
+
+# Change the elf files to bin.
+for elfFile in *.elf; do 
+    arm-none-eabi-objcopy -O binary "$elfFile" "${elfFile%.elf}.bin"
+done
