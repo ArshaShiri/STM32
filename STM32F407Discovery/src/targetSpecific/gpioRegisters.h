5f901da0d3bf34cdc1fc582632863c3ffb946d89
Arsha <arsha.shiri@gmail.com>
2022-03-29 20:17:37 +0200

    Added other methods to gpipRegisters class for setting OutputType, OutputSpeed, and PullupPullDownControl

--- STM32F407Discovery/src/targetSpecific/gpioRegisters.h
+++ STM32F407Discovery/src/targetSpecific/gpioRegisters.h
@@ -7,12 +7,53 @@
 #include "utils/helpers.h"
 #include "utils/registerAccess.h"
 
-enum class PortMode
+enum class PortMode : std::uint8_t
 {
   input,
   generalpurposeOutput,
   alternateFunction,
   analog
+};
+
+enum class OutputType : bool
+{
+  pushPull,
+  openDrain
+};
+
+enum class OutputSpeed : std::uint8_t
+{
+  low,
+  medium,
+  high,
+  veryHigh
+};
+
+enum class PullupPullDownControl : std::uint8_t
+{
+  noPullupPullDown,
+  pullup,
+  pullDown
+};
+
+enum class AlternateFunction : std::uint8_t
+{
+  af0,
+  af1,
+  af2,
+  af3,
+  af4,
+  af5,
+  af6,
+  af7,
+  af8,
+  af9,
+  af10,
+  af11,
+  af12,
+  af13,
+  af14,
+  af15
 };
 
 template<typename RegisterAddressType>
@@ -36,7 +77,6 @@ public:
   template<PortMode mode, PinType pinNumber>
   static void setPortMode(const RegisterAddressType baseRegisterAddress)
   {
-    static constexpr auto numberOfPins = 16;
     static_assert(pinNumber < numberOfPins, "Pin number is not supported!");
 
     const auto portModeValue = portModeToValue.at(mode);
@@ -45,12 +85,85 @@ public:
                                                                      portModeValue << numberOfShifts);
   }
 
+  template<OutputType type, PinType pinNumber>
+  static void setOutputType(const RegisterAddressType baseRegisterAddress)
+  {
+    static_assert(pinNumber < numberOfPins, "Pin number is not supported!");
+
+    const auto outputTypeValue = outputTypeToValue.at(type);
+    RegisterAccess<RegisterAddressType, RegisterAddressType>::regSet(baseRegisterAddress + Offsets::otyper,
+                                                                     outputTypeValue << pinNumber);
+  }
+
+  template<OutputSpeed speed, PinType pinNumber>
+  static void setOutputSpeed(const RegisterAddressType baseRegisterAddress)
+  {
+    static_assert(pinNumber < numberOfPins, "Pin number is not supported!");
+
+    const auto outputSpeedValue = outputSpeedToValue.at(speed);
+    const auto numberOfShifts = pinNumber * 2;
+    RegisterAccess<RegisterAddressType, RegisterAddressType>::regSet(baseRegisterAddress + Offsets::ospeedr,
+                                                                     outputSpeedValue << numberOfShifts);
+  }
+
+  template<PullupPullDownControl type, PinType pinNumber>
+  static void setPullupPullDown(const RegisterAddressType baseRegisterAddress)
+  {
+    static_assert(pinNumber < numberOfPins, "Pin number is not supported!");
+
+    const auto pullupPullDownValue = pullupPullDownControlToValue.at(type);
+    const auto numberOfShifts = pinNumber * 2;
+    RegisterAccess<RegisterAddressType, RegisterAddressType>::regSet(baseRegisterAddress + Offsets::pupdr,
+                                                                     pullupPullDownValue << numberOfShifts);
+  }
+
 private:
-  static constexpr StaticMap<PortMode, RegisterType, 4> portModeToValue{ { { { PortMode::input, 0b00 },
-                                                                             { PortMode::generalpurposeOutput, 0b01 },
-                                                                             { PortMode::alternateFunction, 0b10 },
-                                                                             { PortMode::analog, 0b11 } } } };
+  static constexpr StaticMap<PortMode, RegisterAddressType, 4> portModeToValue{
+    { { { PortMode::input, 0b00 },
+        { PortMode::generalpurposeOutput, 0b01 },
+        { PortMode::alternateFunction, 0b10 },
+        { PortMode::analog, 0b11 } } }
+  };
+
+  static constexpr StaticMap<OutputType, RegisterAddressType, 2> outputTypeToValue{
+    { { { OutputType::pushPull, 0b0 }, { OutputType::openDrain, 0b1 } } }
+  };
+
+  static constexpr StaticMap<OutputSpeed, RegisterAddressType, 4> outputSpeedToValue{
+    { { { OutputSpeed::low, 0b00 },
+        { OutputSpeed::medium, 0b01 },
+        { OutputSpeed::high, 0b10 },
+        { OutputSpeed::veryHigh, 0b11 } } }
+  };
+
+  static constexpr StaticMap<PullupPullDownControl, RegisterAddressType, 4> pullupPullDownControlToValue{
+    { { { PullupPullDownControl::noPullupPullDown, 0b00 },
+        { PullupPullDownControl::pullup, 0b01 },
+        { PullupPullDownControl::pullDown, 0b10 } } }
+  };
+
+  static constexpr StaticMap<AlternateFunction, RegisterAddressType, 16> alternateFunctionToValue{
+    { { { AlternateFunction::af0, 0b0000 },
+        { AlternateFunction::af1, 0b0001 },
+        { AlternateFunction::af2, 0b0010 },
+        { AlternateFunction::af3, 0b0011 },
+        { AlternateFunction::af4, 0b0100 },
+        { AlternateFunction::af5, 0b0101 },
+        { AlternateFunction::af6, 0b0110 },
+        { AlternateFunction::af7, 0b0111 },
+        { AlternateFunction::af8, 0b1000 },
+        { AlternateFunction::af9, 0b1001 },
+        { AlternateFunction::af10, 0b1010 },
+        { AlternateFunction::af11, 0b1011 },
+        { AlternateFunction::af12, 0b1100 },
+        { AlternateFunction::af13, 0b1101 },
+        { AlternateFunction::af14, 0b1110 },
+        { AlternateFunction::af15, 0b1111 } } }
+  };
+
+  static constexpr auto numberOfPins = 16;
 };
+
 
 class GPIORegistersTarget
 {
--- STM32F407Discovery/tst/drivers/gpioRegistersTest.cpp
+++ STM32F407Discovery/tst/drivers/gpioRegistersTest.cpp
@@ -19,6 +19,9 @@ protected:
   }
 
   [[nodiscard]] RegisterType getValueOfModeRegister() const { return gpioRegisterSet.at(0); }
+  [[nodiscard]] RegisterType getValueOfOutputTypeRegister() const { return gpioRegisterSet.at(1); }
+  [[nodiscard]] RegisterType getValueOfOutputSpeedRegister() const { return gpioRegisterSet.at(2); }
+  [[nodiscard]] RegisterType getValueOfPullupPullDownRegister() const { return gpioRegisterSet.at(3); }
 
   RegisterTypeHost registerSetBaseAddressValue{ 0 };
   static constexpr auto numberOfGPIORegisters{ 10 };
@@ -62,4 +65,64 @@ TEST_F(GPIORegisterTest, setPinFifteenModeToAnalog) // NOLINT: Static storage wa
   const auto numberOfShifts = UINT32_C(pinNumber * 2);
   const auto expectedValue = UINT32_C(0b11) << numberOfShifts;
   EXPECT_EQ(getValueOfModeRegister(), expectedValue);
+}
+
+TEST_F(GPIORegisterTest, setOutputTypeToPushPull) // NOLINT: Static storage warning.
+{
+  const auto pinNumber = 13;
+  GPIORegisters<RegisterTypeHost>::setOutputType<OutputType::pushPull, pinNumber>(registerSetBaseAddressValue);
+
+  const auto expectedValue = UINT32_C(0b0) << pinNumber;
+  EXPECT_EQ(getValueOfOutputTypeRegister(), expectedValue);
+}
+
+TEST_F(GPIORegisterTest, setOutputTypeToOpenDrain) // NOLINT: Static storage warning.
+{
+  const auto pinNumber = 15;
+  GPIORegisters<RegisterTypeHost>::setOutputType<OutputType::openDrain, pinNumber>(registerSetBaseAddressValue);
+
+  const auto expectedValue = UINT32_C(0b1) << pinNumber;
+  EXPECT_EQ(getValueOfOutputTypeRegister(), expectedValue);
+}
+
+TEST_F(GPIORegisterTest, setOutputSpeedToMedium) // NOLINT: Static storage warning.
+{
+  const auto pinNumber = 11;
+  GPIORegisters<RegisterTypeHost>::setOutputSpeed<OutputSpeed::medium, pinNumber>(registerSetBaseAddressValue);
+
+  const auto numberOfShifts = UINT32_C(pinNumber * 2);
+  const auto expectedValue = UINT32_C(0b01) << numberOfShifts;
+  EXPECT_EQ(getValueOfOutputSpeedRegister(), expectedValue);
+}
+
+TEST_F(GPIORegisterTest, setOutputSpeedToVeryHight) // NOLINT: Static storage warning.
+{
+  const auto pinNumber = 15;
+  GPIORegisters<RegisterTypeHost>::setOutputSpeed<OutputSpeed::veryHigh, pinNumber>(registerSetBaseAddressValue);
+
+  const auto numberOfShifts = UINT32_C(pinNumber * 2);
+  const auto expectedValue = UINT32_C(0b11) << numberOfShifts;
+  EXPECT_EQ(getValueOfOutputSpeedRegister(), expectedValue);
+}
+
+TEST_F(GPIORegisterTest, setPullupPullDownToPullDown) // NOLINT: Static storage warning.
+{
+  const auto pinNumber = 7;
+  GPIORegisters<RegisterTypeHost>::setPullupPullDown<PullupPullDownControl::pullDown, pinNumber>(
+    registerSetBaseAddressValue);
+
+  const auto numberOfShifts = UINT32_C(pinNumber * 2);
+  const auto expectedValue = UINT32_C(0b10) << numberOfShifts;
+  EXPECT_EQ(getValueOfPullupPullDownRegister(), expectedValue);
+}
+
+TEST_F(GPIORegisterTest, setPullupPullDownToPullup) // NOLINT: Static storage warning.
+{
+  const auto pinNumber = 3;
+  GPIORegisters<RegisterTypeHost>::setPullupPullDown<PullupPullDownControl::pullup, pinNumber>(
+    registerSetBaseAddressValue);
+
+  const auto numberOfShifts = UINT32_C(pinNumber * 2);
+  const auto expectedValue = UINT32_C(0b01) << numberOfShifts;
+  EXPECT_EQ(getValueOfPullupPullDownRegister(), expectedValue);
 }
