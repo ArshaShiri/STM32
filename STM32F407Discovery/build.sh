0ec16c0f030f127c744e02250c481731f919b744
Arsha <arsha.shiri@gmail.com>
2022-04-07 18:15:19 +0200

    Modified the build script and added options for building host, target or rebuilding.

--- STM32F407Discovery/build.sh
+++ STM32F407Discovery/build.sh
@@ -1,10 +1,56 @@
 #!/bin/sh
-buildDir="./build"
 
-if [ ! -d "$buildDir" ]; then
-	mkdir $buildDir 
+printManual() {
+	echo ""
+	echo -t "\t-t Build for target."
+	echo -h "\t-h Build for host."
+	echo -r "\t-r Rebuild"
+	exit 1
+}
+
+if [ $# -eq 0 ]; then
+	printManual
+	exit
 fi
 
-cmake -S ./ -B $buildDir -DCMAKE_TOOLCHAIN_FILE=./cmake/hostToolchainForUnitTesting.cmake
-cd $buildDir
-make
+while getopts :htr flag; do
+	case "${flag}" in
+	t) buildForTarget=true ;;
+	h) buildForHost=true ;;
+	r) rebuild=true ;;
+	\?)
+		printManual
+		exit
+		;;
+	esac
+done
+
+scriptDir=$PWD
+buildDir="$PWD/build"
+
+if [ "$rebuild" = true ]; then
+	echo "Removing build directory."
+	rm -rf $buildDir
+fi
+
+mkdir -p $buildDir
+
+if [ "$buildForHost" = true ]; then
+	echo "Building for host:"
+
+	buildHostDir="$buildDir/host"
+	mkdir -p $buildHostDir
+	cmake -S ./ -B $buildHostDir -DCMAKE_TOOLCHAIN_FILE=./cmake/hostToolchainForUnitTesting.cmake
+	cd $buildHostDir
+	make
+fi
+
+if [ "$buildForTarget" = true ]; then
+	echo "Building for target:"
+
+	buildTargetDir="$buildDir/target"
+	mkdir $buildTargetDir
+	cmake -S ./ -B $buildTargetDir -DCMAKE_TOOLCHAIN_FILE=./cmake/targetToolchain.cmake
+	cd $buildTargetDir
+	make
+fi
